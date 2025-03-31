c9d20792c4949509874648ffc9d0327a8af92b59
조용원 <auau3@kakao.com>
2025-03-31 09:49:17 +0900

    added dark mode styles to logo

--- components/Header.tsx
+++ components/Header.tsx
@@ -6,7 +6,7 @@ const Header = () => {
   return (
     <header className="sticky top-0 z-50 flex h-24 w-[100%] flex-row items-center justify-center bg-[rgba(255,255,255,0.5)] px-4 py-8 backdrop-blur-md max-md:h-12 max-md:px-6 dark:bg-[rgba(0,0,0,0)]">
       <Link href="/">
-        <WoniBlogLogo width={100} height={24} />
+        <WoniBlogLogo width={100} height={24} className="dark:fill-zinc-200" />
       </Link>
     </header>
   );
